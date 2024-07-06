13c707d7a9eca3bf01d3242559d3eea9c637439b
Simran145678 <harsimrankaur708@gmail.com>
2024-07-06 19:57:56 -0400

    css files configured

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
 import About from "./components/About";
